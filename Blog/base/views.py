9a44a6cb2eb134c64b4233909e839ce8a4344898
hadi <hadinofal1993@gmail.com>
2022-04-17 13:00:24 +0300

    solving-error

--- Blog/base/views.py
+++ Blog/base/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .blog import blog
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
